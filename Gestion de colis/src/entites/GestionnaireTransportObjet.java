0717a9d44f1d92d0fc696a561b37028305b9195a
Hoshigaki <Hoshigaki@192.168.1.13>
2014-06-07 14:17:30 +0200

    same

---
+++ Gestion de colis/src/entites/GestionnaireTransportObjet.java
@@ -0,0 +1,67 @@
+package entites;
+
+import impl.GestionnaireTransportObjetImpl;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import org.omg.CosNaming.NamingContext;
+import org.omg.PortableServer.POA;
+import org.omg.PortableServer.POAHelper;
+
+
+/*******************************
+ * Gestionnaire de transport objet - partie serveur
+ *******************************/
+public class GestionnaireTransportObjet {
+
+	public static void main(String args[]) {
+		try {
+			// Intialisation de l'ORB
+			//************************
+			org.omg.CORBA.ORB orb = org.omg.CORBA.ORB.init(args,null);
+
+			// Gestion du POA
+			//****************
+			// Recuperation du POA
+			POA rootPOA = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+
+			// Creation du servant
+			//*********************
+			GestionnaireTransportObjetImpl monGestionnaireTransportObjet = new GestionnaireTransportObjetImpl(args);
+
+			// Activer le servant au sein du POA et recuperer son ID
+			byte[] monGestionnaireTransportObjetId = rootPOA.activate_object(monGestionnaireTransportObjet);
+
+			// Activer le POA manager
+			rootPOA.the_POAManager().activate();
+
+			
+	        // Enregistrement dans le service de nommage
+	        //*******************************************
+	        // Recuperation du naming service
+	        NamingContext nameRoot=org.omg.CosNaming.NamingContextHelper.narrow(orb.resolve_initial_references("NameService"));
+
+	        // Construction du nom a enregistrer
+	        org.omg.CosNaming.NameComponent[] nameToRegister = new org.omg.CosNaming.NameComponent[1];
+	        System.out.println("Sous quel nom voulez-vous enregistrer l'objet Corba ?");
+	        String nomObj = "GTransportObjet";
+	        nameToRegister[0] = new org.omg.CosNaming.NameComponent(nomObj,"");
+
+	        // Enregistrement de l'objet CORBA dans le service de noms
+	        nameRoot.rebind(nameToRegister,rootPOA.servant_to_reference(monGestionnaireTransportObjet));
+	        System.out.println("==> Nom '"+ nomObj + "' est enregistre dans le service de noms.");
+			 
+			String IORServant = orb.object_to_string(rootPOA.servant_to_reference(monGestionnaireTransportObjet));
+			System.out.println("L'objet possede la reference suivante :");
+			System.out.println(IORServant);
+
+			// Lancement de l'ORB et mise en attente de requete
+			//**************************************************
+			orb.run();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ Gestion de colis/src/entites/ui/OffreTableModel.java
@@ -0,0 +1,65 @@
+package entites.ui;
+
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.table.AbstractTableModel;
+
+import utils.OffreTransport;
+
+public class OffreTableModel extends AbstractTableModel{
+
+    private static final long serialVersionUID = 1L;
+    private ArrayList <OffreTransport> offresTransport;
+
+    
+    public ArrayList <OffreTransport> getOffresTransport() {
+        return offresTransport;
+    }
+
+    public void setOffresTransport(ArrayList <OffreTransport> offresTransport) {
+        this.offresTransport = offresTransport;
+    }
+
+    public OffreTableModel(ArrayList <OffreTransport> offresTransport){
+        this.offresTransport = offresTransport;
+
+    }
+
+    @Override
+    public String getColumnName(int pColumnIndex) {
+        switch(pColumnIndex){
+            case 0 : return "Num Offre";
+            case 1 : return "Station Depart";
+            case 2 : return "Station Arrivee";
+            case 3 : return "Etat";
+        }
+        throw new IllegalArgumentException("Index de colonne invalide ! " + pColumnIndex);
+    }
+
+    public int getColumnCount() {
+        return 4;
+    }
+
+    public int getRowCount() {
+        return this.offresTransport.size();
+    }
+
+    @Override
+    public boolean isCellEditable(int pRowIndex, int pColumnIndex) {
+        // On ne peut pas editer les cellules
+        return false;
+    }
+
+    public Object getValueAt(int pRowIndex, int pColumnIndex) {
+
+        OffreTransport offre = this.offresTransport.get(pRowIndex);
+        switch(pColumnIndex){
+            case 0 : return offre.getNumeroOffre();
+            case 1 : return offre.getNomStationDepart();
+            case 2 : return offre.getNomStationArrivee();
+            case 3 : return offre.getEtatOffreTransport().toString();
+        }
+        throw new IllegalArgumentException("Index de colonne invalide ! " + pColumnIndex);
+    }
+}
